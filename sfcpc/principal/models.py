f81a583606f25f4de50e9fffc1ce6cbe9faf7137
Esteban Lopez <estebanlf92@gmail.com>
2013-11-02 22:24:53 -0800

    modelo producto bien hecho
    zhou noob

--- sfcpc/principal/models.py
+++ sfcpc/principal/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
 # Create your models here.
-<<<<<<< HEAD
 class Producto(models.Model):
 	Nombre = models.CharField(max_length = 50)
-	Clave = models.IntegerField(Field.primary_key = true)
-	Unidad_de_medida = CharField(max_length = 10)
-	Descripcion = CharField(max_length = 50)
-	Precio = models.DecimalField(decimal_places = 2)
+	Clave = models.CharField(primary_key = True, max_length = 12)
+	Unidad_de_medida = models.CharField(max_length = 10)
+	Descripcion = models.CharField(max_length = 50)
+	Precio = models.DecimalField(max_digits = 8, decimal_places = 2)
 	Activo = models.BooleanField()
+	Tipo_PS = (
+                ('P','Producto'),
+                ('S','Servicio'),
+        )
+	Tipo=models.CharField(max_length=1,choices=Tipo_PS)
 
-=======
 class Dolar_peso(models.Model):
     valor=models.FloatField()
     fecha=models.DateTimeField(primary_key=True)
->>>>>>> 839765bc0dafb601d9100963f62de6517868f8aa
