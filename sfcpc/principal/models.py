b4c3e9f9c9d8ba79346818d3f071d320083cfc89
Esteban Lopez <estebanlf92@gmail.com>
2013-11-02 21:50:20 -0800

    modelo producto
    no esta compilado

--- sfcpc/principal/models.py
+++ sfcpc/principal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+	Nombre = models.CharField(max_length = 50)
+	Clave = models.IntegerField(Field.primary_key = true)
+	Unidad_de_medida = CharField(max_length = 10)
+	Descripcion = CharField(max_length = 50)
+	Precio = models.DecimalField(decimal_places = 2)
+	Activo = models.BooleanField()
+
