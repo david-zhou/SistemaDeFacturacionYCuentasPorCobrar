d041e81b9193dde08314039dc7a462fd458350ca
Esteban Lopez <estebanlf92@gmail.com>
2013-11-12 16:18:09 -0800

    modals, bajas y cambios

--- sfcpc/principal/views.py
+++ sfcpc/principal/views.py
@@ -28,6 +28,26 @@ def Clientes_Alta(request):
 		formulario = ClientesForm()
 	return render_to_response("Clientes_Alta.html", {"formulario":formulario} , context_instance = RequestContext(request))
 
+def Clientes_Baja(request):
+	if request.method == "POST":
+		formulario = ClientesForm(request.POST)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect("/Clientes_Baja")
+	else:
+		formulario = ClientesForm()
+	return render_to_response("Clientes_Baja.html", {"formulario":formulario} , context_instance = RequestContext(request))
+
+def Clientes_Cambio(request):
+	if request.method == "POST":
+		formulario = ClientesForm(request.POST)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect("/Clientes_Cambio")
+	else:
+		formulario = ClientesForm()
+	return render_to_response("Clientes_Cambio.html", {"formulario":formulario} , context_instance = RequestContext(request))
+
 def Pagos(request):
 	return render_to_response("Pagos.html" , context_instance = RequestContext(request))
 
@@ -43,6 +63,28 @@ def Productos_Alta(request):
 	else:
 		formulario = ProductosForm()
 	return render_to_response("Productos_Alta.html", {"formulario":formulario} , context_instance = RequestContext(request))
+
+
+def Productos_Baja(request):
+	if request.method == "POST":
+		formulario = ProductosForm(request.POST)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect("/Productos_Baja")
+	else:
+		formulario = ProductosForm()
+	return render_to_response("Productos_Baja.html", {"formulario":formulario} , context_instance = RequestContext(request))
+
+def Productos_Cambio(request):
+	if request.method == "POST":
+		formulario = ProductosForm(request.POST)
+		if formulario.is_valid():
+			formulario.save()
+			return HttpResponseRedirect("/Productos_Cambio")
+	else:
+		formulario = ProductosForm()
+	return render_to_response("Productos_Cambio.html", {"formulario":formulario} , context_instance = RequestContext(request))
+
 
 def Pagos_Factura(request):
 	return render_to_response("Pagos_Factura.html" , context_instance = RequestContext(request))
